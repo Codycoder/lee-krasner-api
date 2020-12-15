4337f10c43635ab41ae95ae0f5ac23793c6c5d40
Codycoder <andykohler99@gmail.com>
2020-12-15 11:05:56 -0500

    cleaned up html and css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 
 
 
+
+
+
+
+
+
 //     const url = "https://www.brooklynmuseum.org/api/v2/leekrasner";
 // fetch(url, {
 //   method: "GET",
@@ -19,31 +25,5 @@
 //     })
 //     .catch(err => console.log(err))
 
-    // ajax({
-    //     url: 'https://www.brooklynmuseum.org/api/v2/leekrasner',
-    //     type: 'GET',
-    //     beforeSend: function (xhr) {
-    //       xhr.setRequestHeader('api_key', 'MQmlbbxap0QRJC8De7ZhTTHB9kkHg2q5');
-    //     },
-    //     data: {},
-    //     success: function () { },
-    //     error: function () { },
-    //   });
 
-
-      var xhr = new XMLHttpRequest()
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    // Get JSON from the returned string
-    const res = JSON.parse(xhr.responseText)
-    // Typical action to be performed when the document is ready:
-    document.getElementById("asideContent").innerHTML = `<h2>${res}</h2>`
-  }
-}
-/*
- ** Notice in this example we are hitting and actual url
- ** and returning whatever we get from the API we're accessing
- */
-xhr.open("GET", "https://www.brooklynmuseum.org/api/v2/", true)
-xhr.send()
      
